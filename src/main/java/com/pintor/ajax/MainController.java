f082a26093bfaf7526bed9019b00b3ad3c3f1478
pintor.dev <pintor.dev@gmail.com>
2023-08-03 12:37:28 +0900

    ajax #2

--- src/main/java/com/pintor/ajax/MainController.java
+++ src/main/java/com/pintor/ajax/MainController.java
@@ -3,6 +3,7 @@ package com.pintor.ajax;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -18,9 +19,6 @@ public class MainController {
 
     @GetMapping("/index")
     public String index(Model model) {
-
-        count1++;
-        count2++;
         model.addAttribute("count1", count1);
         model.addAttribute("count2", count2);
 
@@ -30,13 +28,26 @@ public class MainController {
     @GetMapping("/count1")
     @ResponseBody
     public RsData<Integer> getCount1(Model model) {
-        count1++;
+        count1 = 0;
         return new RsData<>("S-1", "current count1", count1);
     }
 
     @GetMapping("/count2")
     @ResponseBody
     public RsData<Integer> getCount2(Model model) {
-        return new RsData<>("S-1", "current count2", count2);
+        count2 = 0;
+        return new RsData<>("S-2", "current count2", count2);
+    }
+
+    @PostMapping("/increaseCount1")
+    @ResponseBody
+    public RsData<Integer> increaseCount1(Model model) {
+        return new RsData<>("S-1", "count1 increased", ++count1);
+    }
+
+    @PostMapping("/increaseCount2")
+    @ResponseBody
+    public RsData<Integer> increaseCount2(Model model) {
+        return new RsData<>("S-2", "count2 increased", ++count2);
     }
 }
